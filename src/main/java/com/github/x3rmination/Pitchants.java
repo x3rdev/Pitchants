a1292d9de4501c6bc73e39f15b4ba5e0a87d0973
X3RMINATION <nicolaeungur@gmail.com>
2021-10-21 19:27:20 -0700

    Release 1.1 - fixed issues

--- src/main/java/com/github/x3rmination/Pitchants.java
+++ src/main/java/com/github/x3rmination/Pitchants.java
@@ -29,7 +29,7 @@ public class Pitchants {
 
     public static final String MODID = "pitchants";
     public static final String NAME = "Pitchants";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2]";
     public static final Logger LOGGER = LogManager.getLogger();
 
